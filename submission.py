5321edbb72e62ab723924e383d00b413c53dae20
Jonathan Hartley <tartley@tartley.com>
2014-05-23 12:56:16 +0100

    My response
    Move import to top.
    Add 'main' driver to demonstrate.
    Still unepected Nones in output.

--- submission.py
+++ submission.py
@@ -1,9 +1,12 @@
+import random as rd
+
 class RandomSequence(object):
-    import random as rd
+
     def __init__(self,_values,_probabilities):
         self._values = _values
         self._probabilities = _probabilities
         pass
+
     def next(self):
         tick,total,index = rd.random(),0.0,0
         for i in self._probabilities:
@@ -11,5 +14,19 @@ class RandomSequence(object):
             total += i
             if total >= tick:
                 break
-        return(self._values[index-1])
+            return(self._values[index-1])
+
+
+def main():
+    seq1 = RandomSequence([111, 222], [0.2, 0.8])
+    seq2 = RandomSequence([3, 4], [0.2, 0.8])
+    print seq1.next()
+    print seq1.next()
+    print seq1.next()
+    print seq1.next()
+    print seq1.next()
+    print seq1.next()
+    
+if __name__ == '__main__':
+    main()
 
