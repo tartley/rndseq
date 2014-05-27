d626262dad7a750699829c76211af4571ca57d81
Jonathan Hartley <tartley@tartley.com>
2014-05-27 20:17:50 +0100

    Miguel's fortest addions

--- submission.py
+++ submission.py
@@ -1,4 +1,6 @@
 import random as rd
+import bisect
+import time
 
 
 class RandomSequence(object):
@@ -9,43 +11,20 @@ class RandomSequence(object):
     def __init__(self,_values,_probabilities):
         self._values = _values
         self._probabilities = _probabilities
+        self.sum_p,self.total = [],0
+        for i in self._probabilities:
+            self.total += i
+            self.sum_p.append(self.total)
+        if len(self._values) == 0 or len(self._probabilities)== 0:
+            raise AssertionError("Values or/and Probabilities vector/s is/are empty")
+        if sum(self._probabilities) != 1.0:
+            raise ArithmeticError("Probabilities don't add up to 1")
 
     def next(self):
         # chooses a weighted random number from the first vector (values)
         # Weights are correponding floats in second vector (probabilities).
-        tick,total,index = rd.random(),0.0,0
-        for i in self._probabilities:
-            index += 1
-            total += i
-            if total >= tick:
-                break
-        return(self._values[index-1])
+        out_value = bisect.bisect(self.sum_p,rd.random())
 
+        return(self._values[out_value])
 
-def main():
-    # Number of individual prints for seq1.next():
-    repetitions = 10
-    # Large sampling number to check for probabilities:
-    Large_Rep = 1000000
-    # Ocurrences of each value:
-    Counter_111 = 0
-    Counter_222 = 0
-
-    seq1 = RandomSequence([111,222],[0.1,0.9])
-    
-    print  str(repetitions) +" Individual Repetitions of .next()"
-    for i in range(repetitions):
-        print (seq1.next())
-    print "Large sampling for .next()"
-    for i in range(Large_Rep):
-        if seq1.next() == 111:
-            Counter_111 += 1
-        if seq1.next() == 222:
-            Counter_222 += 1
-
-    print "111 occurrences: " + str(float(Counter_111)*100/Large_Rep) + "%"
-    print "222 occurrences: " + str(float(Counter_222)*100/Large_Rep) + "%"
-  
-if __name__ == '__main__':
-    main()
 
--- test_submission.py
+++ test_submission.py
@@ -1,17 +1,20 @@
+import submission_fortest as sft
 import unittest
 
-from submission import RandomGen
-
-
-class TestSubmission(unittest.TestClass):
+class TestSubmission(unittest.TestCase):
 
     def test_RandomGen_raises_on_empty_distribution(self):
         with self.assertRaises(AssertionError):
-            RandomGen([], [])
+            sft.RandomSequence([], [])
 
     def test_next_num_returns_only_possible_output(self):
-        gen = RandomGen([123], [1])
-        self.assertEqual(gen.next_num(), 123)
-        self.assertEqual(gen.next_num(), 123)
-        self.assertEqual(gen.next_num(), 123)
+        gen = sft.RandomSequence([123], [1])
+        self.assertEqual(gen.next(), 123)
+        self.assertEqual(gen.next(), 123)
+        self.assertEqual(gen.next(), 123)
 
+    def test_RandomGen_raises_on_sum_probabilities_not_one(self):
+        with self.assertRaises(ArithmeticError):
+            sft.RandomSequence([111,222,333],[0.5,0.5,0.5])
+        with self.assertRaises(ArithmeticError):
+            sft.RandomSequence([111,222,333],[0.3,0.3,0.3])
