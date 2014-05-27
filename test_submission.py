7a0748e206d5cefefb018cfea6a39082d0d4d500
Jonathan Hartley <tartley@tartley.com>
2014-05-27 21:12:15 +0100

    replace import alias with more appropriate spelling

--- test_submission.py
+++ test_submission.py
@@ -1,22 +1,22 @@
 import unittest
 
-import submission as sft
+import submission as sub
 
 class TestSubmission(unittest.TestCase):
 
     def test_RandomGen_raises_on_empty_distribution(self):
         with self.assertRaises(AssertionError):
-            sft.RandomSequence([], [])
+            sub.RandomSequence([], [])
 
     def test_next_num_returns_only_possible_output(self):
-        gen = sft.RandomSequence([123], [1])
+        gen = sub.RandomSequence([123], [1])
         self.assertEqual(gen.next(), 123)
         self.assertEqual(gen.next(), 123)
         self.assertEqual(gen.next(), 123)
 
     def test_RandomGen_raises_on_sum_probabilities_not_one(self):
         with self.assertRaises(ArithmeticError):
-            sft.RandomSequence([111,222,333],[0.5,0.5,0.5])
+            sub.RandomSequence([111,222,333],[0.5,0.5,0.5])
         with self.assertRaises(ArithmeticError):
-            sft.RandomSequence([111,222,333],[0.3,0.3,0.3])
+            sub.RandomSequence([111,222,333],[0.3,0.3,0.3])
 
