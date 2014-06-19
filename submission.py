f9f19f12a89267a7e4c4dbaaca44141a3f25636c
Jonathan Hartley <tartley@tartley.com>
2014-06-19 09:54:55 +0100

    Jonathan: whitespace only, PEP8 conformance.
    Add space after commas.
    Remove inconstantly used blank lines.
    Put test for normal (non exception) behaviour first.

--- submission.py
+++ submission.py
@@ -1,17 +1,16 @@
 import bisect
 import random as rd
 
-
 class RandomSequence(object):
 
     # Takes an object of two vectors
     # First one is a sel of values
     # Second is a set of probabilities
-    def __init__(self,_values,_probabilities,RF = rd.random):
+    def __init__(self, _values, _probabilities, RF = rd.random):
         self.RF = RF
         self._values = _values
         self._probabilities = _probabilities
-        self.sum_p,self.total,LV,LP = [],0,len(self._values),len(self._probabilities)
+        self.sum_p, self.total, LV, LP = [], 0, len(self._values), len(self._probabilities)
         for i in self._probabilities:
             self.total += i
             self.sum_p.append(self.total)
@@ -23,11 +22,9 @@ class RandomSequence(object):
         if sum(self._probabilities) != 1.0:
             raise ArithmeticError("Probabilities don't add up to 1")
 
-
-
     def next(self):
         # chooses a weighted random number from the first vector (values)
         # Weights are correponding floats in second vector (probabilities).
-        out_value = bisect.bisect_left(self.sum_p,self.RF())
-
+        out_value = bisect.bisect_left(self.sum_p, self.RF())
         return(self._values[out_value])
+
--- test_submission.py
+++ test_submission.py
@@ -4,47 +4,45 @@ import submission as sub
 
 class TestSubmission(unittest.TestCase):
 
-    def test_RandomGen_raises_on_empty_distribution(self):
-        with self.assertRaises(AssertionError):
-            sub.RandomSequence([], [])
-
     def test_next_num_returns_only_possible_output(self):
         gen = sub.RandomSequence([123], [1])
         self.assertEqual(gen.next(), 123)
         self.assertEqual(gen.next(), 123)
         self.assertEqual(gen.next(), 123)
 
+    def test_RandomGen_raises_on_empty_distribution(self):
+        with self.assertRaises(AssertionError):
+            sub.RandomSequence([], [])
+
     def test_RandomGen_raises_on_sum_probabilities_not_one(self):
         with self.assertRaises(ArithmeticError):
-            sub.RandomSequence([111,222,333],[0.5,0.5,0.5])
+            sub.RandomSequence([111, 222, 333], [0.5, 0.5, 0.5])
         with self.assertRaises(ArithmeticError):
-            sub.RandomSequence([111,222,333],[0.3,0.3,0.3])
+            sub.RandomSequence([111, 222, 333], [0.3, 0.3, 0.3])
 
     def test_RandomGen_raises_on_lists_not_the_same_size(self):
         with self.assertRaises(AssertionError):
-            sub.RandomSequence([111,222,333],[0.1,0.9])
+            sub.RandomSequence([111, 222, 333], [0.1, 0.9])
 
     def test_next_num_output_depends_on_tick_position(self):
-
-        gen = sub.RandomSequence([123,456],[0.1,0.9],lambda :0.0)
-        self.assertEqual(gen.next(),123)
-        self.assertEqual(gen.next(),123)
-        self.assertEqual(gen.next(),123)
-        gen = sub.RandomSequence([123,456],[0.1,0.9],lambda :0.08)
-        self.assertEqual(gen.next(),123)
-        self.assertEqual(gen.next(),123)
-        self.assertEqual(gen.next(),123)
-        gen = sub.RandomSequence([123,456],[0.1,0.9],lambda :0.1)
-        self.assertEqual(gen.next(),123)
-        self.assertEqual(gen.next(),123)
-        self.assertEqual(gen.next(),123)
-        gen = sub.RandomSequence([123,456],[0.1,0.9],lambda :0.8)
-        self.assertEqual(gen.next(),456)
-        self.assertEqual(gen.next(),456)
-        self.assertEqual(gen.next(),456)
-        gen = sub.RandomSequence([123,456],[0.1,0.9],lambda :1.0)
-        self.assertEqual(gen.next(),456)
-        self.assertEqual(gen.next(),456)
-        self.assertEqual(gen.next(),456)
-
+        gen = sub.RandomSequence([123, 456], [0.1, 0.9], lambda :0.0)
+        self.assertEqual(gen.next(), 123)
+        self.assertEqual(gen.next(), 123)
+        self.assertEqual(gen.next(), 123)
+        gen = sub.RandomSequence([123, 456], [0.1, 0.9], lambda :0.08)
+        self.assertEqual(gen.next(), 123)
+        self.assertEqual(gen.next(), 123)
+        self.assertEqual(gen.next(), 123)
+        gen = sub.RandomSequence([123, 456], [0.1, 0.9], lambda :0.1)
+        self.assertEqual(gen.next(), 123)
+        self.assertEqual(gen.next(), 123)
+        self.assertEqual(gen.next(), 123)
+        gen = sub.RandomSequence([123, 456], [0.1, 0.9], lambda :0.8)
+        self.assertEqual(gen.next(), 456)
+        self.assertEqual(gen.next(), 456)
+        self.assertEqual(gen.next(), 456)
+        gen = sub.RandomSequence([123, 456], [0.1, 0.9], lambda :1.0)
+        self.assertEqual(gen.next(), 456)
+        self.assertEqual(gen.next(), 456)
+        self.assertEqual(gen.next(), 456)
 
