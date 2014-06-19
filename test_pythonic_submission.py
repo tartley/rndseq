e0410488514d00531104d58ed98776e9ef121a75
Jonathan Hartley <tartley@tartley.com>
2014-06-19 11:00:32 +0100

    test_pythonic_... needs to call next() on generator objects

--- test_pythonic_submission.py
+++ test_pythonic_submission.py
@@ -6,7 +6,7 @@ class TestSubmission(unittest.TestCase):
 
     def test_RandomGen_raises_on_empty_distribution(self):
         with self.assertRaises(AssertionError):
-            sub.RandomSequence([], [])
+            list(sub.RandomSequence([], []))
 
     def test_next_num_returns_only_possible_output(self):
         gen = sub.RandomSequence([123], [1])
@@ -16,13 +16,13 @@ class TestSubmission(unittest.TestCase):
 
     def test_RandomGen_raises_on_sum_probabilities_not_one(self):
         with self.assertRaises(ArithmeticError):
-            sub.RandomSequence([111,222,333],[0.5,0.5,0.5])
+            list(sub.RandomSequence([111,222,333],[0.5,0.5,0.5]))
         with self.assertRaises(ArithmeticError):
-            sub.RandomSequence([111,222,333],[0.3,0.3,0.3])
+            list(sub.RandomSequence([111,222,333],[0.3,0.3,0.3]))
 
     def test_RandomGen_raises_on_lists_not_the_same_size(self):
         with self.assertRaises(AssertionError):
-            sub.RandomSequence([111,222,333],[0.1,0.9])
+            list(sub.RandomSequence([111,222,333],[0.1,0.9]))
 
     def test_next_num_output_depends_on_tick_position(self):
 
